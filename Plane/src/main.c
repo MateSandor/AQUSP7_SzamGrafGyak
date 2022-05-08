b49ad4007c89156c2a21f895d8769e85f0c75da3
MateSandor <79148317+MateSandor@users.noreply.github.com>
2022-05-08 21:29:02 +0200

    Plane Project Upload

---
+++ Plane/src/main.c
@@ -0,0 +1,22 @@
+#include "app.h"
+
+#include <stdio.h>
+
+/**
+ * Main function
+ */
+int main(int argc, char* argv[])
+{
+    App app;
+
+    init_app(&app, 1300, 700);
+    while (app.is_running) {
+        SDL_ShowCursor(SDL_DISABLE);
+        handle_app_events(&app);
+        update_app(&app);
+        render_app(&app);
+    }
+    destroy_app(&app);
+
+    return 0;
+}
